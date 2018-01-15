918901f7efdf8a201bd8bfc408f092695d051872
plesiosaure <42@e-xode.com>
2018-01-15 23:43:54 +0100

    canvas

--- src/App.js
+++ src/App.js
@@ -8,35 +8,58 @@ class App extends Component {
     this.state = {
       counter: 0,
       running: null,
+      chrono: '0 : 00 : 00 : 0'
     }
   }
+  
   start = () => {
     this.setState({
-      running: setInterval(this.increment, 100)
+      running: setInterval(this.display, 100)
     });
   }
-  increment = () => {
+  
+  pause = () => {
+    clearInterval(this.state.running);
+  }
+  
+  reset = () => {
+    this.pause();
     this.setState({
+      counter: 0,
+      chrono: '0 : 00 : 00 : 0'
+    });
+    this.pause();
+    
+    
+  } 
+  
+  display = () => {
+    let hours = Math.floor(this.state.counter / (60 * 60 * 10));
+    let minutes = Math.floor((this.state.counter / ( 60 * 10)) % 60 );
+    let seconds = Math.floor((this.state.counter / 10) % 60);
+    let dseconds = this.state.counter % 10;
+    this.setState({
+      chrono: hours + ' : ' + minutes.toString().padStart(2,'0') + ' : ' + seconds.toString().padStart(2,'0') + ' : ' + dseconds,
       counter: this.state.counter + 1
     });
     
   }
-  pause = () => {
-    clearInterval(this.state.running);
+  componentDidMount = () =>{
+    let canvas = this.refs.canvas;
+    let ctx = canvas.getContext("2d");
+    let radius = canvas.height / 2;
+    ctx.translate(radius, radius);
+    radius = radius * 0.90;
+    this.drawClock(ctx, radius);
+    }
+
+  drawClock = (ctx, radius) => {
+    ctx.arc(0, 0, radius, 0 , 2*Math.PI);
+    ctx.fillStyle = "black";
+    ctx.fill();
+    //https://www.w3schools.com/graphics/canvas_clock_face.asp
   }
-  reset = () => {
-    this.setState({
-      counter: 0
-    });
-    
-  } 
-  display = () => {
-    let hours = this.state.counter % (60 * 60 * 10);
-    let minutes = this.state.counter %  60 * 10);
-    let seconds = this.state.counter % (10);
-    let dseconds = ;
-    return this.state.counter
-  }
+  
   render() {
     return (
       <div className="App">
@@ -50,7 +73,9 @@ class App extends Component {
         <button onClick={this.start}>start</button>
         <button onClick={this.pause}>pause</button>
         <button onClick={this.reset}>reset</button>
-        <div>{this.display()}</div>
+        <div>{this.state.chrono}</div>
+        <canvas ref="canvas" width={400} height={400} style={{backgroundColor: "#FFF"}}></canvas>
+
       </div>
     );
   }
